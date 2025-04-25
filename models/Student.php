64bba133281d77c5802cb15784e7ec91ab85848d
Jethro <jethro.garcia@clsu2.edu.ph>
2025-04-25 18:57:59 +0800

    create student model

---
+++ models/Student.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once 'Model.php';
+
+class Student extends Model
+{
+  protected static $table = 'students';
+
+  public $id;
+  public $student_id;
+  public $name;
+  public $gender;
+  public $birthdate;
+  public $course_id;
+  public $year_level;
+  public $status;
+  public $created_at;
+  public $updated_at;
+
+  public function __construct(array $data = [])
+  {
+    foreach ($data as $key => $value) {
+      if (property_exists($this, $key)) {
+        $this->$key = $value;
+      }
+    }
+  }
+
+  public static function all()
+  {
+    $result = parent::all();
+    return $result ? array_map(fn($data) => new self($data), $result) : null;
+  }
+
+  public static function find($id)
+  {
+    $result = parent::find($id);
+    return $result ? new self($result) : null;
+  }
+
+  public static function create($data)
+  {
+    $result = parent::create($data);
+    return $result ? new self($result) : null;
+  }
+
+  public function update($data)
+  {
+    $result = parent::updateById($this->id, $data);
+
+    if ($result) {
+      foreach ($data as $key => $value) {
+        if (property_exists($this, $key)) {
+          $this->$key = $value;
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function save()
+  {
+    $data = [
+      'student_id' => $this->student_id,
+      'name' => $this->name,
+      'gender' => $this->gender,
+      'birthdate' => $this->birthdate,
+      'course_id' => $this->course_id,
+      'year_level' => $this->year_level,
+      'status' => $this->gender,
+    ];
+    $this->update($data);
+  }
+
+  public function delete()
+  {
+    $result = parent::deleteById($this->id);
+
+    if ($result) {
+      foreach ($this as $key => $value) {
+        if (property_exists($this, $key)) {
+          unset($this->$key);
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
