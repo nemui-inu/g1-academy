81cb4a98590560b5972a6ec4e660fbba6a167264
Jethro <jethro.garcia@clsu2.edu.ph>
2025-04-25 12:01:12 +0800

    create database model

---
+++ models/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database {
+  private $host = 'localhost';
+  private $user = 'root';
+  private $password = 'root';
+  private $dbname = 'g1_academy';
+  private $conn;
+
+  public function __construct() {
+    try {
+      $db = "mysql:host=$this->host;dbname=$this->dbname;charset=utf8mb4";
+      $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_PERSISTENT => false
+      ];
+      $this->conn = new PDO($db, $this->user, $this->password, $options);
+    } catch (PDOException $e) {
+      echo "(!) Connection failed: " . $e->getMessage();
+    }
+  }
+
+  public function setConnection($conn) {
+    $this-> conn = $conn;
+  }
+
+  public function getConnection() {
+    return $this->conn;
+  }
+
+  public function destruct() {
+    $this->conn = null;
+  }
+}
