5e9ed348014d9c7e82ed7d1d5ae1bcb14b9d34eb
Jethro <jethro.garcia@clsu2.edu.ph>
2025-04-29 11:36:05 +0800

    modify header and initialize login page

--- layout/header.php
+++ layout/header.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>G1 Academy</title>
-  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 <body>
   
---
+++ views/auth/login.php
@@ -0,0 +1,9 @@
+<?php
+include '../../layout/header.php';
+?>
+
+<h1>Hello</h1>
+
+<?php
+include '../../layout/footer.php';
+?>
